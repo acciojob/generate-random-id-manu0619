6fdec11c85e5d3bd07991e585aba6ce4bc451d4a
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-08 19:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+  let res = "";
+  for (let i = 0; i < l; i++) {
+    // Generate random digit (0-9)
+    let a = Math.floor(Math.random() * 10); 
+    // Convert the digit to a character (as a number string)
+    let s = String.fromCharCode(65 + a); // '48' is the ASCII value for '0'
+    res += s;
+  }
+  return res;
 }
 
 // Do not change the code below.
